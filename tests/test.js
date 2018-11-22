168f09654aa2a26d2a0335fe40400f01fb9872ce
Plainweaver <editor@plainweaver.com>
2018-11-23 06:07:26 +0900

    Update abstract-leveldown@6.0

--- tests/test.js
+++ tests/test.js
@@ -3,26 +3,19 @@
 require('es5-shim');
 
 var tape   = require('tape');
-var localstorage = require('../');
-var testCommon = require('./testCommon');
-var testBuffer = new Buffer('hello');
+var suite = require('abstract-leveldown/test');
+var LocalStorage = require('../');
 
-require('abstract-leveldown/abstract/leveldown-test').args(localstorage, tape);
-require('abstract-leveldown/abstract/open-test').args(localstorage, tape, testCommon);
-require('abstract-leveldown/abstract/del-test').all(localstorage, tape, testCommon);
-require('abstract-leveldown/abstract/put-test').all(localstorage, tape, testCommon);
-require('abstract-leveldown/abstract/get-test').all(localstorage, tape, testCommon);
-require('abstract-leveldown/abstract/put-get-del-test').all(
-  localstorage, tape, testCommon, testBuffer);
-require('abstract-leveldown/abstract/close-test').close(localstorage, tape, testCommon);
-require('abstract-leveldown/abstract/iterator-test').all(localstorage, tape, testCommon);
+var dbi = 0;
 
-require('abstract-leveldown/abstract/chained-batch-test').all(localstorage, tape, testCommon);
-require('abstract-leveldown/abstract/approximate-size-test').setUp(localstorage, tape, testCommon);
-require('abstract-leveldown/abstract/approximate-size-test').args(localstorage, tape, testCommon);
-
-require('abstract-leveldown/abstract/ranges-test').all(localstorage, tape, testCommon);
-require('abstract-leveldown/abstract/batch-test').all(localstorage, tape, testCommon);
-
-require('./custom-tests.js').all(localstorage, tape, testCommon);
-
+suite({
+    test: tape,
+    factory: function() {
+        return new LocalStorage(String(++dbi));
+    },
+    createIfMissing: false,
+    errorIfExists: false,
+    seek: false,
+    bufferKeys: false,
+    snapshots: false,
+});
